a7caad59a5102e27b0a69519d2c620432a30d51e
1ke4real <mikeeleman@gmail.com>
2022-08-05 12:12:10 +0200

    conditional

---
+++ Basics/conditional/conditional.js
@@ -0,0 +1,8 @@
+let javascriptName = prompt("What's the official name of Javascript ?");
+
+if (javascriptName === "ECMAScript"){
+    alert("Right");
+}
+else {
+    alert("False");
+}
---
+++ Basics/conditional/display_the_sign.js
@@ -0,0 +1,11 @@
+let number = prompt("write number");
+
+if (number > 0) {
+    alert('1');
+}
+else if (number < 0) {
+    alert('-1');
+}
+else {
+    alert('0');
+}
